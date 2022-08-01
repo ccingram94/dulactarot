a839cae68ee1977fa06a703c253a3e18d63881c4
Constancegit status <constanceingram94@gmail.com>
2022-08-01 10:50:44 -0600

    updating database to prisma postgres hosted by heroku

--- components/header.js
+++ components/header.js
@@ -1,15 +1,29 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import UserIcon from '@heroicons/react/solid/UserCircleIcon'
+import { useSession, signIn, signOut } from 'next-auth/react'
 
 export default function Header() {
+  const { date: session, status } = useSession()
   return (
     <div>
         <div className="w-screen bg-teal-900">
-            <p className="p-2 font-montserrat text-blue-200 text-center"> ✨ free tarot readings with AI artwork ✨ </p>
+            <p className="p-2 font-montserrat text-blue-200 text-center"> ✨ tarot readings with AI artwork inspired by fairytale illustrator Edmund Dulac ✨ </p>
         </div>
-        <div className="w-screen text-center bg-banner bg-cover bg-bottom flex flex-row justify-between">
-          <div className="w-screen bg-teal-900 bg-opacity-90">
-            <h1 className="p-6 pt-12 text-white font-bebas text-2xl md:text-4xl lg:text-6xl">✨ D u l a c   T a r o t ✨</h1>
+        <div className="flex flex-row flex-wrap w-screen text-center justify-center bg-banner bg-cover bg-bottom">
+          <div className="flex flex-col lg:flex-row text-center justify-between items-center w-screen bg-teal-900 bg-opacity-90 pt-6">
+            <div className="flex flex-row lg:w-1/3 font-bebas text-yellow-500 opacity-60 text-center justify-center">
+              <p className="text-2xl p-2">Meanings</p>
+              <p className="text-2xl p-2">Readings</p>
+              <p className="text-2xl p-2">Journal</p>
+            </div>
+            <div className="flex lg:w-1/3 justify-center p-4">
+              <h1 className="text-white font-bebas text-2xl lg:text-4xl xl:text-5xl">✨ D u l a c   T a r o t ✨</h1>
+            </div>
+            <div className="flex flex-row lg:w-1/3 justify-center p-4">
+              <UserIcon className="w-12 text-yellow-400 opacity-60" onClick={signIn()} />
+              <button className="hidden lg:block text-amber-900 bg-amber-300 opacity-50 rounded-xl font-bebas text-xl xl:text-2xl p-4 m-4">sign in</button>
+            </div>
           </div>
         </div>
     </div>
--- components/threecard.js
+++ components/threecard.js
@@ -28,9 +28,9 @@ export default function ThreeCard() {
   }
 
   function assignCards () {
-    var result1 = Math.floor(Math.random() * 79)
-    var result2 = Math.floor(Math.random() * 79)
-    var result3 = Math.floor(Math.random() * 79)
+    var result1 = Math.floor(Math.random() * 156)
+    var result2 = Math.floor(Math.random() * 156)
+    var result3 = Math.floor(Math.random() * 156)
     dispatch(assignCard1(result1));
     dispatch(assignCard2(result2));
     dispatch(assignCard3(result3));
@@ -46,75 +46,75 @@ export default function ThreeCard() {
                 {!cardsDealt && 
                 <motion.div transition={{ delay: 0.1 }} initial={{ opacity: 0 }} animate={{ opacity: 1}} exit={{ opacity: 0}} className="rounded-xl max-w-xs p-12">
                   <div onClick={() => dealCards()} className="rounded-xl bg-yellow-300 hover:shadow-2xl hover:shadow-yellow-300 transition-all ease-linear">
-                    <Image src={back} className="rounded-xl bg-yellow-300"/>
+                    <Image src={back} className="rounded-xl bg-yellow-300" width="300" height="500"/>
                   </div>
                   <div>
                     {!cardsDealt &&
-                      <p>click the deck to deal your cards</p>
+                      <p className="font-montserrat text-amber-300 md:text-xl opacity-40 p-6">Reveal your fate.  Click the deck to deal your cards.</p>
                     }
 
                   </div>
                 </motion.div>
                 }
-                {cardsDealt && 
-                      <div>
-                        <button>Save Reading</button>
-                        <button>New Reading</button>
-                      </div>
-                  }
               </div>
               <div>
                 {cardsDealt && 
                 <div>
-                  <motion.div className="flex flex-row flex-wrap justify-center" initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ staggerChildren: 0.5, duration: 1.0, delay: 0.3}}>
+                  <motion.div className="flex flex-row flex-wrap justify-center min-h-screen" initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ staggerChildren: 0.5, duration: 1.0, delay: 0.3}}>
                         <div className="max-w-xs p-12" onClick={ () => setCard1Flip(true) } >
                           {!card1flip &&
-                            <div className="max-w-sx">
-                              <h2 className="text-xl p-2">Past</h2>
-                              <Image src={back} className="rounded-xl" width="300" height="500"/>
-                            </div>
+                            <motion.div className="max-w-xs" initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ staggerChildren: 0.5, duration: 1.0, delay: 0.3}}>
+                              <h2 className="font-bebas text-xl p-2">Past</h2>
+                              <div className="rounded-xl bg-yellow-300 hover:shadow-2xl hover:shadow-yellow-300 transition-all ease-linear">
+                                <Image src={back} className="rounded-xl" width="300" height="500"/>
+                              </div>
+                            </motion.div>
                           }
                           {card1flip &&
-                            <div className="max-w-xs">
-                              <h2 className="text-xl p-2">Past</h2>
-                              <Image src={ cards[0].image } width="300" height="500" className="rounded-xl" />
+                            <motion.div className="max-w-xs" initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ staggerChildren: 0.5, duration: 1.0, delay: 0.3}}>
+                              <h2 className="text-xl p-2 font-bebas">Past</h2>
+                              <Image src={ cards[card1].image } width="300" height="500" className="rounded-xl" />
                               <p className="text-xl p-2 text-yellow-500">{ cards[card1].name}</p>
                               <p className="text-md p-2 text-yellow-600"> { cards[card1].description } </p>
-                            </div>
+                            </motion.div>
                           }
                         </div>
                         <div className="max-w-xs p-12" onClick={ () => setCard2Flip(true) } >
                           {!card2flip &&
-                            <div>
-                              <h2 className="text-xl p-2">Past</h2>
-                              <Image src={back} className="rounded-xl" width="300" height="500" />
-                            </div>
+                            <motion.div className="max-w-xs" initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ staggerChildren: 0.5, duration: 1.0, delay: 0.3}}>
+                              <h2 className="text-xl p-2 font-bebas">Present</h2>
+                              <div className="rounded-xl bg-yellow-300 hover:shadow-2xl hover:shadow-yellow-300 transition-all ease-linear">
+                                <Image src={back} className="rounded-xl" width="300" height="500"/>
+                              </div>
+                            </motion.div>
                           }
                           {card2flip &&
-                            <div>
-                              <h2 className="text-xl p-2">Past</h2>
-                              <Image src={ cards[0].image } width="300" height="500" className="rounded-xl" />
+                            <motion.div className="max-w-xs" initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ staggerChildren: 0.5, duration: 1.0, delay: 0.3}}>
+                              <h2 className="text-xl p-2 font-bebas">Present</h2>
+                              <Image src={ cards[card2].image } width="300" height="500" className="rounded-xl" />
                               <div>
                                 <p className="text-xl p-2 text-yellow-500">{ cards[card2].name}</p>
                                 <p className="text-md p-2 text-yellow-600"> { cards[card2].description } </p>
                               </div>
-                            </div>
+                            </motion.div>
                           }
                         </div>
                         <div className="max-w-xs p-12" onClick={ () => setCard3Flip(true) } >
                           {!card3flip &&
-                            <div>
-                              <h2 className="text-xl p-2">Past</h2>
-                              <Image src={back} className="rounded-xl" width="300" height="500" />
-                            </div>
+                            <motion.div className="max-w-xs" initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ staggerChildren: 0.5, duration: 1.0, delay: 0.3}}>
+                              <h2 className="text-xl p-2 font-bebas">Future</h2>
+                              <div className="rounded-xl bg-yellow-300 hover:shadow-2xl hover:shadow-yellow-300 transition-all ease-linear">
+                                <Image src={back} className="rounded-xl" width="300" height="500"/>
+                              </div>
+                            </motion.div>
                           }
                           {card3flip &&
-                            <div>
-                              <h2 className="text-xl p-2">Past</h2>
-                              <Image src={ cards[0].image } width="300" height="500" className="rounded-xl" />
+                            <motion.div className="max-w-xs" initial={{ opacity: 0 }} animate={{ opacity: 1}} transition={{ staggerChildren: 0.5, duration: 1.0, delay: 0.3}}>
+                              <h2 className="text-xl p-2 font-bebas">Future</h2>
+                              <Image src={ cards[card3].image } width="300" height="500" className="rounded-xl" />
                               <p className="text-xl p-2 text-yellow-500">{ cards[card3].name}</p>
                               <p className="text-md p-2 text-yellow-600"> { cards[card3].description } </p>
-                            </div>
+                            </motion.div>
                           }
                         </div>
                   </motion.div>
