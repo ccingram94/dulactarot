d5445e94d017c26464c563e018a6bb08030f5bcc
Constancegit status <constanceingram94@gmail.com>
2022-08-05 06:10:41 -0600

    adding some detail pages

---
+++ pages/majorarcana.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import Header from '../components/header'
+import AllCardMeanings from '../components/allcardmeanings'
+
+export default function Meanings() {
+  return (
+    <div>
+      <Head>
+        <title>Dulac Tarot</title>
+        <meta name="description" content="free tarot reading with AI artwork" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main className="bg-yellow-200 text-black font-montserrat ">
+        <h2 className="text-4xl lg:text-6xl font-bebas text-center p-2 pt-12">Major Arcana</h2>
+        <div className="flex flex-row flex-wrap justify-center">
+            <p className="max-w-sm text-center p-6"><i>The 22 major arcana cards of the tarot are often seen as the most important cards in a tarot deck, as they represent the Fool's journey through life, from start to finish. The major arcana cards can represent both positive and negative aspects of a person's life, and often show the ups and downs that a person will experience.</i></p>
+        </div>
+        <div className="flex flex-col flex-wrap justify-center p-4 bg-teal-900 text-white font-bebas text-xl md:text-2xl lg:text-4xl">
+        </div>
+        <AllCardMeanings />
+      </main>
+    </div>
+  )
+}
--- pages/minorarcana.js
+++ pages/minorarcana.js
@@ -14,8 +14,11 @@ export default function Meanings() {
       </Head>
       <Header />
       <main className="bg-yellow-200 text-black font-montserrat">
-        <h2 className="text-4xl lg:text-6xl font-bebas text-center p-2 pt-12">Tarot Card Meanings</h2>
-        <p className="text-xl text-center pb-6">the ancient art revealed</p>
+        <h2 className="text-4xl lg:text-6xl font-bebas text-center p-2 pt-12">Minor Arcana</h2>
+        <div className="flex flex-row flex-wrap justify-center p-4">
+            <p className="max-w-sm text-center p-4"><i>The minor arcana cards of the tarot represent the smaller details and events of our lives. They can represent our day-to-day thoughts, feelings, and actions. The minor arcana can also give us insight into our subconscious mind and our hidden desires.</i></p>
+            <p className="max-w-sm text-center p-4"><i>The suit of wands represents our creative energy, our passion, and our drive. The suit of cups represents our emotions, our relationships, and our spiritual connection. The suit of swords represents our mental energy, our intellect, and our ability to reason. The suit of pentacles represents our material wealth, our physical health, and our practical skills.</i></p>
+        </div> 
         <div className="flex flex-col flex-wrap justify-center p-4 bg-teal-900 text-white font-bebas text-xl md:text-2xl lg:text-4xl">
           <div className="flex flex-row flex-wrap justify-center">
             <Link href="/majorarcana">
