34cd3987599ae00f0fc50b75aec5f7c1f5d88aa5
Constancegit status <constanceingram94@gmail.com>
2022-08-05 05:30:38 -0600

    adding some nav sections

--- pages/meanings.js
+++ pages/meanings.js
@@ -15,6 +15,30 @@ export default function Meanings() {
       <main className="bg-yellow-200 text-black font-montserrat">
         <h2 className="text-4xl lg:text-6xl font-bebas text-center p-2 pt-12">Tarot Card Meanings</h2>
         <p className="text-xl text-center pb-6">the ancient art revealed</p>
+        <div className="flex flex-col flex-wrap justify-center p-4 bg-teal-900 text-white font-bebas text-xl md:text-2xl lg:text-4xl">
+          <div className="flex flex-row flex-wrap justify-center">
+            <div className="p-4 m-4 bg-black rounded-xl">
+              <h3>Major Arcana</h3>
+            </div>
+            <div className="p-4 m-4 bg-black rounded-xl">
+              <h3>Minor Arcana</h3>
+            </div>
+          </div>
+          <div className="flex flex-row flex-wrap justify-center">
+            <div className="p-4 m-4 bg-black rounded-xl">
+              <h3>Cups</h3>
+            </div>
+            <div className="p-4 m-4 bg-black rounded-xl">
+              <h3>Coins</h3>
+            </div>
+            <div className="p-4 m-4 bg-black rounded-xl">
+              <h3>Swords</h3>
+            </div>
+            <div className="p-4 m-4 bg-black rounded-xl">
+              <h3>Wands</h3>
+            </div>
+          </div>
+        </div>
         <AllCardMeanings />
       </main>
     </div>
