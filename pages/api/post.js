ed5264780491148f29a5c17c602b5938ff769c8a
Constancegit status <constanceingram94@gmail.com>
2022-08-07 04:21:04 -0600

    prisma post

--- pages/api/post.js
+++ pages/api/post.js
@@ -7,19 +7,14 @@ export default async function handle(req, res) {
     const resultdata = req.body.readingresults[0];
     const session = await getSession({ req });
     if (session) {
-        try {
-            const success = await prisma.reading.create({
-                data: {
+        const success = await prisma.reading.create({
+            data: {
                     result: resultdata,
                     type: typedata,
                     author: { connect: { email: session?.user?.email } },
                 },
-            })
-            console.log(success)
-        } catch(error) {
-            console.log(error)
-        }
-        console.log(req.body)
+            });
+        console.log(success)
         res.json(success);
     } else {
         res.status(401).send({ message: 'unauthorized' });
