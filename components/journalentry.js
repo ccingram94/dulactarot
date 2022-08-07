be23fcf081583866691e7f5ddf5f6fc9ae4eb987
Constancegit status <constanceingram94@gmail.com>
2022-08-07 02:21:27 -0600

    fixed journalentry component, was previously mapping indices instead of actual results

--- components/journalentry.js
+++ components/journalentry.js
@@ -5,9 +5,12 @@ import { format, parseJSON } from 'date-fns'
 export default function JournalEntry (props) {
     const [ caution, setCaution ] = useState(false);
     const [ deleted, setDeleted ] = useState(false);
+    const [ deleting, setDeleting ] = useState(false);
     const deleteItem = props.props.id;
 
     const deleteReading = async() => {
+      setCaution(false);
+      setDeleting(true);
         const readingId = deleteItem;
         try {
           const body = {readingId};
@@ -20,6 +23,7 @@ export default function JournalEntry (props) {
         } catch (error) {
           console.error(error)
         }
+        setDeleting(false);
       }
 
     if (deleted == true) {
@@ -32,31 +36,33 @@ export default function JournalEntry (props) {
 
     return (
         <div>
-            <button onClick={console.log(props.props.result)}>hello world</button>
             <h2 className="font-bebas text-xl lg:text-2xl text-center">{format(parseJSON(props.props.createdAt), 'PPPP')}</h2>
-            {props.props.type == 'yesno' && <h3>Yes or No</h3>}
-            {props.props.type == 'pastpresentfuture' && <h3>Past, Present, Future</h3>}
-            {props.props.type == 'celticcross' && <h3>Celtic Cross</h3>}
+            {props.props.type == 'yesno' && <h3>✨ Yes or No ✨</h3>}
+            {props.props.type == 'pastpresentfuture' && <h3>✨ Past, Present, Future ✨</h3>}
+            {props.props.type == 'celticcross' && <h3>✨ Celtic Cross ✨</h3>}
             <div className="flex flex-row flex-wrap justify-center p-2 max-w-xl">
-                  { cards.map( (card) => {
-                    if (card.id in props.props.result) {
+                  { props.props.result.map( (result) => {
                       return (
-                        <div key={card.id} className="p-2">
-                            <img src={card.image} className="rounded-xl h-40"/>
-                          <h3 className="font-bebas text-xl lg:text-2xl p-2">{card.name}</h3>
+                        <div key={cards[result].id} className="p-2 flex flex-col justify-center items-center">
+                            <img src={cards[result].image} className="rounded-xl h-40 max-w-fit"/>
+                          <h3 className="font-bebas text-xl lg:text-2xl p-2">{cards[result].name}</h3>
                         </div>
                       );
-                    }
                   })}
             </div>
             <div>
                 {!caution &&
-                    <button onClick={() => setCaution(true)} className="bg-yellow-200 bg-opacity-20 hover:bg-opacity-80 p-2 rounded-xl">delete</button>
+                    <button onClick={() => setCaution(true)} className="bg-yellow-200 bg-opacity-20 hover:bg-opacity-80 p-2 rounded-xl transition-all">delete</button>
                 }
                 {caution &&
                     <div>
                         <p className="pb-2">you sure?</p>
-                        <button onClick={() => deleteReading()} className="bg-red-300 bg-opacity-20 hover:bg-opacity-80 p-2 rounded-xl">yes, delete!</button>
+                        <button onClick={() => deleteReading()} className="bg-red-300 bg-opacity-20 hover:bg-opacity-80 p-2 rounded-xl transition-all">yes, delete!</button>
+                    </div>
+                }
+                {deleting &&
+                    <div>
+                        <button className="bg-red-300 bg-opacity-20 hover:bg-opacity-80 p-2 rounded-xl transition-all">deleting...</button>
                     </div>
                 }
             </div>
