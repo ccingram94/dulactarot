eff25cd0178100dd831c619e506017ca2ca9fd23
Constancegit status <constanceingram94@gmail.com>
2022-08-07 04:57:04 -0600

    trying to add loader between props

--- pages/journal.js
+++ pages/journal.js
@@ -31,48 +31,8 @@ export const getServerSideProps = async() => {
 
 
 export default function Journal(props) {
-  const { data: session, status } = useSession()
-  const [ loading, setLoading ] = useState(true);
+  const { data: session, status } = useSession();
   const [ deleteItem, setDeleteItem ] = useState(null);
-
-  useEffect(() => {
-    if (props) {
-      setLoading(false);
-    }
-  }, [props])
-
-  if (loading) {
-    return (
-      <div>
-        <Head>
-          <title>Dulac Tarot</title>
-          <meta name="description" content="free tarot reading with AI artwork" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main className="bg-floral bg-cover min-h-screen text-black font-montserrat">
-          <div className="bg-yellow-200 min-h-screen bg-opacity-80">
-            <h2 className="text-4xl lg:text-6xl font-bebas text-center p-2 pt-12">My Tarot Journal</h2>
-            <p className="text-xl text-center pb-6">your story, written in the stars</p>
-            <div className="p-2">
-              
-            {status === 'authenticated' &&
-              <div className="flex flex-row flex-wrap justify-center text-center">
-                {loading && <div>loading your journal...</div>}
-                </div>
-              }
-              {status === 'unauthenticated' &&
-                <div className="flex flex-col flex-wrap justify-center text-center items-center">
-                  <p className="p-2 lg:p-4">sign in to save readings to your journal:</p>
-                  <button onClick={() => signIn()} className="bg-black text-white p-2 lg:p-4 w-fit rounded-xl hover:shadow-2xl transition-all">Sign In</button>
-                </div>
-              }
-            </div>
-          </div>
-        </main>
-      </div>
-    )
-  }
-
 
   if (props) {
     return (
@@ -110,5 +70,34 @@ export default function Journal(props) {
       </div>
     )
   }
-  
+
+return (
+    <div>
+      <Head>
+        <title>Dulac Tarot</title>
+        <meta name="description" content="free tarot reading with AI artwork" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="bg-floral bg-cover min-h-screen text-black font-montserrat">
+        <div className="bg-yellow-200 min-h-screen bg-opacity-80">
+          <h2 className="text-4xl lg:text-6xl font-bebas text-center p-2 pt-12">My Tarot Journal</h2>
+          <p className="text-xl text-center pb-6">your story, written in the stars</p>
+          <div className="p-2">
+            
+          {status === 'authenticated' &&
+            <div className="flex flex-row flex-wrap justify-center text-center">
+              {loading && <div>loading your journal...</div>}
+              </div>
+            }
+            {status === 'unauthenticated' &&
+              <div className="flex flex-col flex-wrap justify-center text-center items-center">
+                <p className="p-2 lg:p-4">sign in to save readings to your journal:</p>
+                <button onClick={() => signIn()} className="bg-black text-white p-2 lg:p-4 w-fit rounded-xl hover:shadow-2xl transition-all">Sign In</button>
+              </div>
+            }
+          </div>
+        </div>
+      </main>
+    </div>
+  )
 }
