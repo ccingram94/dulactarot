3b794aaa376e78a20d15322014e37e72827155d0
Constancegit status <constanceingram94@gmail.com>
2022-08-07 04:01:35 -0600

    loading and auth stuff

--- pages/journal.js
+++ pages/journal.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import AllCardMeanings from '../components/allcardmeanings'
 import JournalEntry from '../components/journalentry'
 import { getSession } from 'next-auth/react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import prisma from '../lib/prisma'
 import { format, parseJSON } from 'date-fns'
 import { cards } from '../cards.js'
@@ -32,41 +32,80 @@ export default function Journal(props) {
   const [ loading, setLoading ] = useState(true);
   const [ deleteItem, setDeleteItem ] = useState(null);
 
+  useEffect(() => {
+    if (props) {
+      setLoading(false);
+    }
+  }, [props])
+
+  if (!props) {
+    return (
+      <div>
+        <Head>
+          <title>Dulac Tarot</title>
+          <meta name="description" content="free tarot reading with AI artwork" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className="bg-floral bg-cover min-h-screen text-black font-montserrat">
+          <div className="bg-yellow-200 min-h-screen bg-opacity-80">
+            <h2 className="text-4xl lg:text-6xl font-bebas text-center p-2 pt-12">My Tarot Journal</h2>
+            <p className="text-xl text-center pb-6">your story, written in the stars</p>
+            <div className="p-2">
+              
+            {status === 'authenticated' &&
+              <div className="flex flex-row flex-wrap justify-center text-center">
+                {loading && <div>loading your journal...</div>}
+                </div>
+              }
+              {status === 'unauthenticated' &&
+                <div className="flex flex-col flex-wrap justify-center text-center items-center">
+                  <p className="p-2 lg:p-4">sign in to save readings to your journal:</p>
+                  <button onClick={() => signIn()} className="bg-black text-white p-2 lg:p-4 w-fit rounded-xl hover:shadow-2xl transition-all">Sign In</button>
+                </div>
+              }
+            </div>
+          </div>
+        </main>
+      </div>
+    )
+  }
+
+
   if (props) {
-    setLoading(false);
+    return (
+      <div>
+        <Head>
+          <title>Dulac Tarot</title>
+          <meta name="description" content="free tarot reading with AI artwork" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className="bg-floral bg-cover min-h-screen text-black font-montserrat">
+          <div className="bg-yellow-200 min-h-screen bg-opacity-80">
+            <h2 className="text-4xl lg:text-6xl font-bebas text-center p-2 pt-12">My Tarot Journal</h2>
+            <p className="text-xl text-center pb-6">your story, written in the stars</p>
+            <div className="p-2">
+              
+            {status === 'authenticated' &&
+              <div className="flex flex-row flex-wrap justify-center text-center">
+                {loading && <div>loading your journal...</div>}
+                {props.readings.map((reading) => (
+                  <div className="p-4 m-2 flex flex-col bg-white bg-opacity-90 rounded-xl h-fit" key={reading.id}>
+                    <JournalEntry props={reading} />
+                  </div>
+                ))}
+                </div>
+              }
+              {status === 'unauthenticated' &&
+                <div className="flex flex-col flex-wrap justify-center text-center items-center">
+                  <p className="p-2 lg:p-4">sign in to save readings to your journal:</p>
+                  <button onClick={() => signIn()} className="bg-black text-white p-2 lg:p-4 w-fit rounded-xl hover:shadow-2xl transition-all">Sign In</button>
+                </div>
+              }
+            </div>
+          </div>
+        </main>
+      </div>
+    )
   }
   
-  return (
-    <div>
-      <Head>
-        <title>Dulac Tarot</title>
-        <meta name="description" content="free tarot reading with AI artwork" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="bg-floral bg-cover min-h-screen text-black font-montserrat">
-        <div className="bg-yellow-200 min-h-screen bg-opacity-80">
-          <h2 className="text-4xl lg:text-6xl font-bebas text-center p-2 pt-12">My Tarot Journal</h2>
-          <p className="text-xl text-center pb-6">your story, written in the stars</p>
-          <div className="p-2">
-            {loading && <div>loading your journal...</div>}
-          {status === 'authenticated' &&
-            <div className="flex flex-row flex-wrap justify-center text-center">
-              {props.readings.map((reading) => (
-                <div className="p-4 m-2 flex flex-col bg-white bg-opacity-90 rounded-xl h-fit" key={reading.id}>
-                  <JournalEntry props={reading} />
-                </div>
-              ))}
-              </div>
-            }
-            {status != 'authenticated' &&
-              <div className="flex flex-col flex-wrap justify-center text-center items-center">
-                <p className="p-2 lg:p-4">sign in to save readings to your journal:</p>
-                <button onClick={() => signIn()} className="bg-black text-white p-2 lg:p-4 w-fit rounded-xl hover:shadow-2xl transition-all">Sign In</button>
-              </div>
-            }
-          </div>
-        </div>
-      </main>
-    </div>
-  )
 }
