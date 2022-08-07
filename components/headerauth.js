0233fdc0a9de9eb1487e29964b873d36f23c7808
Constancegit status <constanceingram94@gmail.com>
2022-08-07 00:41:14 -0600

    trying to fix header spacing issues around icons

--- components/headerauth.js
+++ components/headerauth.js
@@ -17,7 +17,7 @@ export default function HeaderAuth() {
     } 
 
     return (
-        <div className="flex flex-row lg:w-1/3 justify-center p-2">
+        <div className="flex flex-row lg:w-1/3 justify-center">
             <UserIcon className="w-8 lg:w-12 text-yellow-400 opacity-60" onClick={() => signIn()}/>
         </div>
     )
--- components/hero.js
+++ components/hero.js
@@ -7,7 +7,7 @@ export default function Hero () {
   return (
     <div className='flex flex-col justify-center'>
       <div className='bg-floral bg-center flex flex-col justify-center'>
-        <h2 className="bg-yellow-200 bg-opacity-80 text-4xl lg:text-6xl font-bebas text-center p-2 pt-12">Free Tarot Card Readings</h2>
+        <h2 className="bg-yellow-200 bg-opacity-80 text-4xl lg:text-6xl font-bebas text-center p-6 lg:p-12">Free Tarot Card Readings</h2>
       </div>
       <FreeReadings />
     </div>
