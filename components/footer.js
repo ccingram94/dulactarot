4287dc63b4218eaec5698b920106bdf1e5132700
Constancegit status <constanceingram94@gmail.com>
2022-08-07 01:11:40 -0600

    fixed spacing issue on footer sections

--- components/footer.js
+++ components/footer.js
@@ -10,26 +10,26 @@ export default function Footer() {
   const { date: session, status } = useSession()
   return (
     <div className="bg-banner bg-cover w-screen min-h-full border-t-2 border-yellow-200">
-        <div className="flex flex-col flex-wrap w-screen text-center justify-center bg-teal-900 bg-opacity-90">
+        <div className="flex flex-col flex-wrap w-screen text-center justify-center items-center bg-teal-900 bg-opacity-90">
           <div>
             <Link href="/">
                 <h1 className="pt-12 text-white font-bebas text-xl lg:text-2xl xl:text-3xl">✨ D u l a c   T a r o t ✨</h1>
             </Link>
           </div>
-          <div className="flex flex-row flex-wrap justify-center p-4 m-2">
-            <div className="text-white text-center">
+          <div className="flex flex-row flex-wrap justify-between max-w-lg p-4 m-2">
+            <div className="text-white text-center p-2 lg:p-4">
                 <h4 className="p-4 font-bebas text-xl lg:text-2xl">My Tarot</h4>
                 <p>Profile</p>
                 <p>Journal</p>
                 <p>Subscription</p>
             </div>
-            <div className="text-white text-center">
+            <div className="text-white text-center p-2 lg:p-4">
                 <h4 className="p-4 font-bebas text-xl lg:text-2xl">Explore</h4>
                 <p>Tarot Card Readings</p>
                 <p>Tarot Card Meanings</p>
                 <p>Blog</p>
-            </div>å
-            <div className="text-white text-center">
+            </div>
+            <div className="text-white text-center p-2 lg:p-4">
                 <h4 className="p-4 font-bebas text-xl lg:text-2xl">Site</h4>
                 <p>About Us</p>
                 <p>Privacy Policy</p>
