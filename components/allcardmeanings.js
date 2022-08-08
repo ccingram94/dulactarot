96f8b3a90eba2e9bc3046ab3cd5b05dfcede72a9
Constancegit status <constanceingram94@gmail.com>
2022-08-07 22:28:32 -0600

    css

--- components/allcardmeanings.js
+++ components/allcardmeanings.js
@@ -16,7 +16,7 @@ export default function AllCardMeanings () {
                 var cardstring = (card.id).toString()
                 var linkstring = "/cards/" + cardstring
                 return (
-                  <div key={card.id} className="bg-black p-2 lg:m-2 rounded-xl max-w-xs">
+                  <div key={card.id} className="bg-black p-2 m-2 rounded-xl max-w-xs">
                     <Image src={card.image} height="250" width="150" layout="responsive" className="rounded-xl"/>
                     <h3 className="font-bebas text-xl lg:text-3xl p-3 pt-6 lg:pt-12">{card.name}</h3>
                     <p className="p-2">{card.description}</p>
