77a3b3d17bb052e7f93994813ce02810fcb923a6
Constancegit status <constanceingram94@gmail.com>
2022-08-07 22:12:56 -0600

    css

--- pages/meanings.js
+++ pages/meanings.js
@@ -12,10 +12,10 @@ export default function Meanings() {
         <meta name="description" content="free tarot reading with AI artwork" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-floral bg-bottom text-black font-montserrat">
+      <main className="bg-floral bg-bottom text-black font-montserrat p-4">
         <h2 className="bg-yellow-200 bg-opacity-60 text-4xl lg:text-6xl font-bebas text-center p-2 pt-12">Tarot Card Meanings</h2>
         <p className="bg-yellow-200 bg-opacity-60 text-xl text-center pb-6">the ancient art revealed</p>
-        <div className="flex flex-col flex-wrap justify-center p-4 m-4 bg-teal-900 text-white font-bebas text-xl md:text-2xl lg:text-4xl">
+        <div className="flex flex-col flex-wrap justify-center p-4 bg-teal-900 text-white font-bebas text-xl md:text-2xl lg:text-4xl">
           <div className="flex flex-row flex-wrap justify-center">
             <Link href="/majorarcana">
               <div className="p-2 m-4 bg-yellow-200 text-black rounded-xl hover:shadow-2xl hover:shadow-yellow-200 transition-all">
@@ -29,7 +29,7 @@ export default function Meanings() {
             </Link>
           </div>
         </div>
-        <div className="bg-teal-900 p-2 m-2">
+        <div className="bg-teal-900 p-2">
           <SearchCards />
         </div>
         <AllCardMeanings />
