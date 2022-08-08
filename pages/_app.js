0dadc038f951a6852a8039230ffdc5a7a37a11d4
Constancegit status <constanceingram94@gmail.com>
2022-08-07 20:27:52 -0600

    changed loader to be at root level instead of component level

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,48 @@
 import '../styles/globals.css'
+import Router from 'next/router'
+import { useEffect, useState } from 'react'
 import { SessionProvider } from 'next-auth/react'
 import { store } from '../store'
 import { Provider } from 'react-redux'
 import Header from '../components/header'
 import Footer from '../components/footer'
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  return (
-    <Provider store={store}>
-      <SessionProvider session={session}>
-        <Header />
-          <Component {...pageProps} />
-        <Footer />
-      </SessionProvider>
-    </Provider>
-  )
+export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  const [ loading, setLoading ] = useState(false);
+  
+  useEffect(() => {
+    const start = () => {
+      console.log("start");
+      setLoading(true);
+    };
+    const end = () => {
+      console.log("finished");
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+      };
+  }, []);
+    return (
+      <>
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <>
+            <Provider store={store}>
+            <SessionProvider session={session}>
+              <Header />
+                <Component {...pageProps} />
+              <Footer />
+            </SessionProvider>
+          </Provider>
+          </>
+        )}
+      </>
+    );
 }
-
-export default MyApp
