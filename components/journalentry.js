f02fa3ad1b74d8edc11797692946791ffafdec04
Constancegit status <constanceingram94@gmail.com>
2022-08-10 03:52:38 -0600

    logic stuff

--- components/journalentry.js
+++ components/journalentry.js
@@ -33,7 +33,7 @@ export default function JournalEntry (props) {
         setDeleting(false);
       }
 
-      const editReading = () => {
+      function editReading () {
         showEdit(false);
         setEditing(true);
       }
