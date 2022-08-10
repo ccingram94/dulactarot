24c6d08f219dc17d19456cbbd52864c9c8ff44da
Constancegit status <constanceingram94@gmail.com>
2022-08-10 01:54:13 -0600

    adding subscription stuff

--- components/signupsection.js
+++ components/signupsection.js
@@ -1,12 +1,22 @@
 import Head from 'next/head'
 import { signIn } from 'next-auth/react'
 
+
+
 export default function SignUpSection() {
+
+    const submitOrder = async() => {
+        const response = await fetch(`https://buy.stripe.com/test_4gw4ha30JcIAeCk9AA`, {
+
+        })
+    }
+
     return (
         <div className="bg-floral bg-cover">
              
             <div className="bg-yellow-200 bg-opacity-80 p-6">
-                <h2 className="p-4 pt-12 text-black text-center font-bebas text-2xl lg:text-6xl">Sign Up for a Free Account</h2>
+                <h2 className="p-4 pt-12 text-black text-center font-bebas text-2xl lg:text-6xl">✨ Sign Up for a Magical Account✨ </h2>
+                <p>Save readings and journal notes, get a daily tarot horoscope, and much more with a $2.99 Dulac Gold account.</p>
                 <div className="flex flex-row flex-wrap justify-center text-center items-center">
                     <button onClick={() => signIn()} className="bg-black text-white max-w-fit p-4 m-2 text-2xl font-bold rounded-xl">SIGN IN</button>
                     <button onClick={() => signIn()} className="bg-black text-white max-w-fit p-4 m-2 text-2xl font-bold rounded-xl">SIGN UP</button>
