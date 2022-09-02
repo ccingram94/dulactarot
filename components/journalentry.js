e4fade98be38aa52170376a9d6c98f53ca145551
Constancegit status <constanceingram94@gmail.com>
2022-09-02 07:24:37 -0600

    journal entry css

--- components/journalentry.js
+++ components/journalentry.js
@@ -102,13 +102,13 @@ export default function JournalEntry (props) {
               <textarea input="text" label="editingtext" onChange={(e) => {setNewNotes(e.target.value)}} className="p-4 m-2 focus:outline-none text-center">{newnotes}</textarea>
             </div>
             }
-            <div className="flex flex-col flex-wrap justify-center text-center align-center content-center p-2 max-w-xl max-h-96 overflow-auto">
+            <div className="flex flex-col justify-center text-center align-center content-center p-2 max-w-xl max-h-96 overflow-auto">
                   { props.props.result.map( (result) => {
                       return (
                         <div key={cards[result].id} className="p-2 flex flex-col justify-center items-center align-center text-center max-w-lg max-h-96 overflow-auto">
                           <img src={cards[result].image} className="rounded-xl h-40 max-w-fit"/>
                           <h3 className="font-bebas text-lg xl:text-xl p-2 max-w-xs">{cards[result].name}</h3>
-                          <p className="text-xs xl:text-sm max-w-sm">{cards[result].description}</p>
+                          <p className="text-xs xl:text-sm">{cards[result].description}</p>
                         </div>
                       );
                   })}
