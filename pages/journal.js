e8af926c0f34b3d885f66b61a2f0d50f09697ad2
Constancegit status <constanceingram94@gmail.com>
2022-09-02 05:36:23 -0600

    journal page css

--- pages/journal.js
+++ pages/journal.js
@@ -59,7 +59,7 @@ export default function Journal(props) {
               <div className="flex flex-row flex-wrap justify-center text-center">
                   <div>
                     {props.readings.map((reading) => (
-                      <div className="p-4 m-2 flex flex-col bg-white bg-opacity-90 rounded-xl h-fit" key={reading.id}>
+                      <div className="p-4 m-2 flex flex-row flex-wrap bg-white bg-opacity-90 rounded-xl h-fit" key={reading.id}>
                         <JournalEntry props={reading} />
                       </div>
                     ))}
