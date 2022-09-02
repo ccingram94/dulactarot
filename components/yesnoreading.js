8f7183494d24d838556a7f61539371d414c775c0
Constancegit status <constanceingram94@gmail.com>
2022-09-02 05:29:26 -0600

    added an update button

--- components/yesnoreading.js
+++ components/yesnoreading.js
@@ -98,7 +98,10 @@ export default function YesNoReading() {
                             <button onClick={() => saveReading()} className="p-2 rounded-xl text-lg md:text-xl  opacity-40 hover:opacity-90 hover:text-teal-900 hover:bg-white hover:bg-opacity-30 transition-all">Save Reading</button>
                           }
                           {saved &&
+                          <div>
                             <button onClick={() => saveReading()} className="p-2 rounded-xl text-lg md:text-xl  opacity-40 hover:opacity-90 hover:text-teal-900 hover:bg-white hover:bg-opacity-30 transition-all">Saved! ✨</button>
+                            <button onClick={() => saveReading()} className="p-2 rounded-xl text-lg md:text-xl  opacity-40 hover:opacity-90 hover:text-teal-900 hover:bg-white hover:bg-opacity-30 transition-all">Update</button>
+                          </div>
                           }
                         </div>
                       }
