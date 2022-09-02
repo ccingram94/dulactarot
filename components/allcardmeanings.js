d5d93d029aa904f47326a563759e69e578cfdf48
Constancegit status <constanceingram94@gmail.com>
2022-09-02 03:54:52 -0600

    decided to make free so deleting stripe stuff and reformatting

--- components/allcardmeanings.js
+++ components/allcardmeanings.js
@@ -16,13 +16,13 @@ export default function AllCardMeanings () {
                 var cardstring = (card.id).toString()
                 var linkstring = "/cards/" + cardstring
                 return (
-                  <div key={card.id} className="bg-black p-2 m-2 rounded-xl max-w-xs">
-                    <Image src={card.image} height="250" width="150" layout="responsive" className="rounded-xl"/>
-                    <h3 className="font-bebas text-xl lg:text-3xl p-3 pt-6 lg:pt-12">{card.name}</h3>
-                    <p className="p-2">{card.description}</p>
-                    <p className="p-2">Reversed: {cards[i].description}</p>
+                  <div key={card.id} className="bg-black p-4 m-2 rounded-xl max-w-xs">
+                    <Image src={card.image} height="250" width="150" className="rounded-xl"/>
+                    <h3 className="font-bebas text-md lg:text-xl p-3 pt-6 lg:pt-12">{card.name}</h3>
+                    <p className="p-2 text-sm lg:text-md">{card.description}</p>
+                    <p className="p-2 text-sm lg:text-md">Reversed: {cards[i].description}</p>
                     <Link href={ linkstring }>
-                      <button className="bg-yellow-200 text-black p-4 rounded-xl m-2">More Information</button>
+                      <button className="bg-yellow-200 text-black p-2 rounded-xl m-2 text-sm lg:text-md">More Information</button>
                     </Link>
                   </div>
                 );
