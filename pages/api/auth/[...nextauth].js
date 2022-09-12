cd36d1f887dffd6aba7bc324c630308b63868fa7
Constancegit status <constanceingram94@gmail.com>
2022-09-11 20:20:31 -0600

    goinswitching from Postgres to DynamoDB, thought code wasnt working but was just delayed

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,26 +1,62 @@
+import { DynamoDB } from "@aws-sdk/client-dynamodb"
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb"
+import NextAuth from "next-auth";
+import GoogleProvider from 'next-auth/providers/Google'
+import { DynamoDBAdapter } from "@next-auth/dynamodb-adapter"
 
+const config = {
+  credentials: {
+    accessKeyId: process.env.NEXT_AUTH_AWS_ACCESS_KEY,
+    secretAccessKey: process.env.NEXT_AUTH_AWS_SECRET_KEY,
+  },
+  region: 'us-east-1',
+  tableName: 'dulacprod',
+};
 
-
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import FacebookProvider from "next-auth/providers/facebook"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-
-const prisma = new PrismaClient()
+const client = DynamoDBDocument.from(new DynamoDB(config), {
+  marshallOptions: {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+  },
+})
 
 export default NextAuth({
-  adapter: PrismaAdapter(prisma),
+  // Configure one or more authentication providers
   providers: [
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.NEXT_AUTH_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.NEXT_AUTH_GOOGLE_CLIENT_SECRET,
     }),
+    // ...add more providers here
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  adapter: DynamoDBAdapter(
+    client, 
+    { tableName: 'dulacprod' },
+  ),
+});
 
-})
+
+// import NextAuth from "next-auth"
+// import GoogleProvider from "next-auth/providers/google"
+// import FacebookProvider from "next-auth/providers/facebook"
+// import { PrismaAdapter } from "@next-auth/prisma-adapter"
+// import { PrismaClient } from "@prisma/client"
+
+// const prisma = new PrismaClient()
+
+// export default NextAuth({
+//   adapter: PrismaAdapter(prisma),
+//   providers: [
+//     FacebookProvider({
+//       clientId: process.env.FACEBOOK_CLIENT_ID,
+//       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+//     }),
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//   ],
+//   secret: process.env.NEXTAUTH_SECRET,
+
+// })
