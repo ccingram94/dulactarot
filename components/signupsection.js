d0b3f731226c5985ede099fc28cf6da71bc1cb07
Constancegit status <constanceingram94@gmail.com>
2022-09-11 22:21:57 -0600

    updated website withed with some legal and disclaimer info

--- components/signupsection.js
+++ components/signupsection.js
@@ -15,11 +15,11 @@ export default function SignUpSection() {
         <div className="bg-floral bg-cover">
              
             <div className="bg-teal-800 bg-opacity-90 p-6 text-white">
-                <h2 className="p-4 pt-12 text-white text-center font-bebas text-2xl lg:text-6xl">⚜️ Sign Up for a Magical Account ⚜️ </h2>
-                <p className="text-center p-2">sign up for a bewitching free account with benefits like:</p>
-                <p className="text-center p-2">⚜️ Save readings to your journal!</p>
-                <p className="text-center p-2">⚜️ Add notes to your reading results!</p>
-                <p className="text-center p-2">⚜️ A daily tarot horoscope!</p>
+                <h2 className="p-4 pt-12 text-white text-center font-bebas text-xl lg:text-2xl">⚜️ Sign Up for a Magical Account ⚜️ </h2>
+                <p className="text-center p-2 text-sm lg:text-lg">sign up for a bewitching free account with benefits like:</p>
+                <p className="text-center p-2 text-sm lg:text-lg">⚜️ Save readings to your journal!</p>
+                <p className="text-center p-2 text-sm lg:text-lg">⚜️ Add notes to your reading results!</p>
+                <p className="text-center p-2 text-sm lg:text-lg">⚜️ A daily tarot horoscope!</p>
                 <div className="flex flex-row flex-wrap justify-center text-center items-center p-2">
                     <button onClick={() => signIn()} className="bg-black text-white max-w-fit p-4 m-2 text-2xl font-bold rounded-xl">SIGN UP</button>
                 </div>
